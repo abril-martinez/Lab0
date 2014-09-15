1163cbaf11c814e16ef6b0c8a6ffca6370091e9f
abril-martinez <amartinez28@email.arizona.edu>
2014-09-15 04:12:15 -0700

    small modification

--- lab0.c
+++ lab0.c
@@ -58,6 +58,7 @@ _CONFIG2( IESO_OFF & SOSCSEL_SOSC & WUTSEL_LEG & FNOSC_PRIPLL & FCKSM_CSDCMD & O
 
 #define BAUDRATE         115200       
 #define BRGVAL          ((FCY/BAUDRATE)/16)-1 
+#define SMALL_NUMBER 300
 
 // ******************************************************************************************* //
 
